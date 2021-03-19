ae05465043a47331cc53a6fad6ef70123a4b449c
Bhavya Shukla <73889955+bhavyashukla16@users.noreply.github.com>
2021-03-20 00:17:52 +0530

    Add files via upload

---
+++ component.php
@@ -0,0 +1,28 @@
+<?php 
+    function inputElement($icon, $placeholder, $name, $value){
+
+        $element = " 
+                        <div class=\"input-group mb-2\">
+                            <div class=\"input-group-text bg-warning\">
+                            $icon
+                            </div>
+                        <input type=\"text\" class=\"form-control\" 
+                        name='$name'
+                        value='$value'
+                        id=\"inlineFormInputGroup\" placeholder='$placeholder'>
+                        </div>
+        
+        ";
+        echo $element;
+    }
+
+    function buttonElement($name, $btnid, $styleclass, $text, $attr){
+
+        $btn ="  <button name='$name''$attr' id='$btnid' class='$styleclass'>$text</button>
+            
+        ";
+
+        echo $btn;
+    }
+
+?>
